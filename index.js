70ce47137cf6cf74cfc5909d1d09687f105da4ff
Paulo Souza <42076064+paulosouza4u@users.noreply.github.com>
2025-08-27 21:36:29 -0300

    ok

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import express from 'express';
+//import {createServer} from 'node:https';
 import {createServer} from 'node:http';
 import {fileURLToPath} from 'node:url';
 import {dirname, join} from 'node:path';
 
+// HTTPS
+//const fs = require('node:fs');
+
 const app = express();
 const server = createServer(app);
+
+// HTTPS
+//const serverSsl = createServer(app);
+
+// HTTPS
+// const options = {
+//     key: fs.readFileSync('caminho/para/sua/chave-privada.key'),
+//     cert: fs.readFileSync('caminho/para/seu/certificado.crt')
+// };
+
+// HTTPS
+// serverSsl.createServer(options, (req, res) => {
+//     res.writeHead(200);
+//     res.end('Olá, Mundo Seguro!');
+// }).listen(443, () => {
+//     console.log('Servidor HTTPS rodando na porta 443.');
+// });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
